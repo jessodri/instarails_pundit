6217766430846a805915085cafea1de0ae52cbaf
Jessica <30883219+jessodri@users.noreply.github.com>
2018-04-20 12:04:50 +1000

    Add devise methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'should have matching email' do
+  user = User.new
+  user.email = ENV.fetch('USER_EMAIL')
+  expect(user.email).to eq('jessodri@example.com')
+end
+
+it 'should have an email that is present' do
+
+end
+
+
+
 end
